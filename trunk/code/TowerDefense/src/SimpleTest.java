e92c9f50feee804c8d94ac7a77c4b10b10511318
ethanaa <ethanaa@637b2924-12a4-11df-8c21-85a09827b053>
2010-02-12 02:58:12 +0000

    Added the Javadoc as a URL linking to the slick2d web API. We'll see what happens.

--- trunk/code/TowerDefense/src/SimpleTest.java
+++ trunk/code/TowerDefense/src/SimpleTest.java
@@ -1,8 +1,8 @@
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.AppGameContainer;
 
 public class SimpleTest extends BasicGame
 {
