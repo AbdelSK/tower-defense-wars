3d6ad817b15ef00bbf4aa021623783eeddbaada6
cwoz36 <cwoz36@637b2924-12a4-11df-8c21-85a09827b053>
2010-02-23 22:58:33 +0000

    Added hit points to mob. Default to 100.

--- code/TowerDefense/src/prototype/Mob.java
+++ code/TowerDefense/src/prototype/Mob.java
@@ -5,19 +5,19 @@ import org.newdawn.slick.util.pathfinding.Path;
 
 public class Mob implements Mover
 {
-	public boolean alive;
 	public int xLoc;
 	public int yLoc;
+	public int hitPoints;
 	public Character ch;
 	public Path path;
 	public int step;
 	
 	public Mob(Character ch, int xLoc, int yLoc)
 	{
+		this.hitPoints = 100;
 		this.ch = new Character(ch);
 		this.xLoc = xLoc;
 		this.yLoc = yLoc;
-		this.alive = true;
 		this.path = new Path();
 		this.step = 0;
 	}
