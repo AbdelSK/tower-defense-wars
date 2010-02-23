886dc3384b45f8d3bedda582dbd0ea49b37ea07c
cwoz36 <cwoz36@637b2924-12a4-11df-8c21-85a09827b053>
2010-02-23 23:18:58 +0000

    Towers now have radius and attack speed.

--- code/TowerDefense/src/prototype/Tower.java
+++ code/TowerDefense/src/prototype/Tower.java
@@ -6,10 +6,14 @@ public class Tower
 	public int xLoc;
 	public int yLoc;
 	public Character ch;
+	public int radius;
+	public int attackSpeed;
 	
 	public Tower(Character ch, int xLoc, int yLoc)
 	{
+		this.attackSpeed = 2;
 		this.ch = ch;
+		this.radius = 10;
 		this.xLoc = xLoc;
 		this.yLoc = yLoc;
 		this.alive = true;
