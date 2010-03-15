c26adc61c4853abe037f7e8d1456eab16891a9a8
white.coby <white.coby@637b2924-12a4-11df-8c21-85a09827b053>
2010-03-15 07:57:14 +0000

    Added the GameMap class which is the map for the game. It is set up to allow objects to move in and out of a tile, and to have towers register for tiles.

--- trunk/code/TowerDefense/src/com/teamamerica/games/unicodewars/utils/Event.java
+++ trunk/code/TowerDefense/src/com/teamamerica/games/unicodewars/utils/Event.java
@@ -5,65 +5,95 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import com.teamamerica.games.unicodewars.object.GameObject;
-/**
- * This is the base class for all events.  These will be
- * passed around when different things occur.
- * @author wkerr
- *
- */
-public class Event {
 
-	private EventType _id;	
-	private List<GameObject> _recipients;
+/**
+ * This is the base class for all events. These will be passed around when
+ * different things occur.
+ * 
+ * @author wkerr
+ * 
+ */
+public class Event
+{
 	
+	private EventType _id;
+	private Location _loc;
+	private List<GameObject> _recipients;
 	
 	// TODO We may want to dispacth an event in the future.
 	// private long _dispatchTime;
 	
-	private Map<String,Object> _parameters;
+	private Map<String, Object> _parameters;
 	
-	public Event(EventType id) { 
+	public Event(EventType id, Location loc)
+	{
 		_id = id;
+		_loc = loc;
 		_recipients = new ArrayList<GameObject>();
-		_parameters = new HashMap<String,Object>();
+		_parameters = new HashMap<String, Object>();
 	}
 	
+	public Event(EventType id)
+	{
+		_id = id;
+		_loc = null;
+		_recipients = new ArrayList<GameObject>();
+		_parameters = new HashMap<String, Object>();
+	}
+
 	/**
 	 * Return the ID of this event.
+	 * 
 	 * @return
 	 */
-	public EventType getId() { 
+	public EventType getId()
+	{
 		return _id;
 	}
 	
+	public Location getLocation()
+	{
+		if (_loc != null)
+		{
+			return _loc;
+		}
+		return new Location(-1, -1);
+	}
+
 	/**
 	 * Add a recipient to the list.s
+	 * 
 	 * @param obj
 	 */
-	public void addRecipient(GameObject obj) { 
+	public void addRecipient(GameObject obj)
+	{
 		_recipients.add(obj);
 	}
 	
-	public List<GameObject> getRecipients() { 
+	public List<GameObject> getRecipients()
+	{
 		return _recipients;
 	}
 	
 	/**
-	 * Add the parameter to the list of parameters
-	 * for this event.
+	 * Add the parameter to the list of parameters for this event.
+	 * 
 	 * @param name
 	 * @param obj
 	 */
-	public void addParameter(String name, Object obj) { 
+	public void addParameter(String name, Object obj)
+	{
 		_parameters.put(name, obj);
 	}
 	
 	/**
 	 * Get the value associated with given parameter.
+	 * 
 	 * @param name
 	 * @return
 	 */
-	public Object getValue(String name) {
+	public Object getValue(String name)
+	{
 		return _parameters.get(name);
 	}
 }
--- trunk/code/TowerDefense/src/com/teamamerica/games/unicodewars/utils/Location.java
+++ trunk/code/TowerDefense/src/com/teamamerica/games/unicodewars/utils/Location.java
@@ -1,6 +1,6 @@
 package com.teamamerica.games.unicodewars.utils;
 
-public class Location
+public class Location implements Comparable
 {
 	public int x;
 	public int y;
@@ -9,5 +9,57 @@ public class Location
 	{
 		this.x = x;
 		this.y = y;
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o == null)
+		{
+			return false;
+		}
+		else if (o.getClass() == Location.class)
+		{
+			Location temp = (Location) o;
+			return (temp.x == this.x && temp.y == this.y);
+		}
+		return false;
+	}
+	
+	@Override
+	public int compareTo(Object o)
+	{
+		if (o == null)
+		{
+			return 1;
+		}
+		else if (o.getClass() == Location.class)
+		{
+			Location temp = (Location) o;
+			if (this.y < temp.y)
+			{
+				return -1;
+			}
+			else if (this.y > temp.y)
+			{
+				return 1;
+			}
+			else
+			{
+				if (this.x < temp.x)
+				{
+					return -1;
+				}
+				else if (this.x > temp.x)
+				{
+					return 1;
+				}
+				else
+				{
+					return 0;
+				}
+			}
+		}
+		return 1;
 	}
 }
