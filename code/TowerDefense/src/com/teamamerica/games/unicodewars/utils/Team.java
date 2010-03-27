10c7c963a675bf60f2f8fa6411015aa44595ca9a
cwoz36 <cwoz36@637b2924-12a4-11df-8c21-85a09827b053>
2010-03-27 11:31:37 +0000

    Added income and gold to Team and methods to support them.

--- code/TowerDefense/src/com/teamamerica/games/unicodewars/utils/Team.java
+++ code/TowerDefense/src/com/teamamerica/games/unicodewars/utils/Team.java
@@ -5,14 +5,50 @@ public enum Team
 	Player1(0), Player2(1);
 	
 	private final int index;
+	private int income;
+	private int score;
+	private int gold;
 
 	Team(int index)
 	{
+		this.income = 10;
+		this.score = 0;
+		this.gold = 10;
 		this.index = index;
 	}
 	
 	public int index()
 	{
 		return this.index;
+	}
+	
+	public void addIncome(int x)
+	{
+		this.income += x;
+	}
+	
+	public void addScore(int x)
+	{
+		this.score += x;
+	}
+
+	public void removeGold(int x)
+	{
+		this.gold -= x;
+	}
+	
+	public void addGold(int x)
+	{
+		this.gold += x;
+	}
+	
+	public int getIncome()
+	{
+		return this.income;
+	}
+	
+	public int getGold()
+	{
+		return this.gold;
 	}
 }
