f2621ecdd8a67fb964ddc55cd8ba182b0214799e
cwoz36 <cwoz36@637b2924-12a4-11df-8c21-85a09827b053>
2010-04-01 04:39:22 +0000

    TowerButton class is completely done and working properly.

--- trunk/code/TowerDefense/src/com/teamamerica/games/unicodewars/object/towers/AwesomeButton.java
+++ trunk/code/TowerDefense/src/com/teamamerica/games/unicodewars/object/towers/AwesomeButton.java
@@ -14,24 +14,17 @@ public abstract class AwesomeButton extends Button implements IButtonPressedList
 	private String defaultText;
 	private String tooltipText;
 	
-	public AwesomeButton(String text, String hover, int x, int y)
+	public AwesomeButton()
 	{
 		super();
+	}
+	
+	public void init(String text, String hover, int x, int y)
+	{
 		this.defaultText = text;
 		this.tooltipText = hover;
 		this.setText(text);
 		this.setSize(x, y);
-		this.setMultiline(true);
-		this.setShrinkable(false);
-	}
-	
-	public AwesomeButton()
-	{
-		super();
-		System.out.println("in awesomebutton default constructor");
-		this.defaultText = "default";
-		this.tooltipText = "hover default";
-		this.setSize(128, 128);
 		this.setMultiline(true);
 		this.setShrinkable(false);
 	}
