ccd6937d2f4eb15f84334a30fea1c21057b3741b
cwoz36 <cwoz36@637b2924-12a4-11df-8c21-85a09827b053>
2010-04-04 22:20:40 +0000

    Removed debug code in Tower Button class.

--- code/TowerDefense/src/com/teamamerica/games/unicodewars/gui/TowerButton.java
+++ code/TowerDefense/src/com/teamamerica/games/unicodewars/gui/TowerButton.java
@@ -2,7 +2,6 @@ package com.teamamerica.games.unicodewars.gui;
 
 import org.fenggui.event.ButtonPressedEvent;
 import com.teamamerica.games.unicodewars.object.towers.TowerBase;
-import com.teamamerica.games.unicodewars.object.towers.TowerBase.Type;
 import com.teamamerica.games.unicodewars.system.BB;
 
 public class TowerButton extends AwesomeButton
@@ -18,13 +17,11 @@ public class TowerButton extends AwesomeButton
 	{
 		super.init(text, hover, x, y);
 		this.type = tt;
-		System.out.println("button init: setting to " + type);
 	}
 
 	@Override
 	public void buttonPressed(ButtonPressedEvent arg0)
 	{
-		System.out.println("button pressed: creating " + type);
 		BB.inst().setTowerSelection(this.type);
 	}
 }
