255f86f40ea035143d130a37d4d97dc78c60b496
white.coby <white.coby@637b2924-12a4-11df-8c21-85a09827b053>
2010-05-03 08:23:19 +0000

    Made a hack to fix an issue where the splash state would leave sooner than we would want due to time spent loading the game before the splash state is displayed. Also made it so that the splash state will stay up for at least 1 second even if a key was pressed.

--- code/TowerDefense/src/com/teamamerica/games/unicodewars/states/SplashState.java
+++ code/TowerDefense/src/com/teamamerica/games/unicodewars/states/SplashState.java
@@ -10,25 +10,29 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 import com.teamamerica.games.unicodewars.Main;
+import com.teamamerica.games.unicodewars.utils.Timer;
 
-public class SplashState extends BHGameState {
+public class SplashState extends BHGameState
+{
 
 	private Image _splashImage;
 	private Music _splashTheme;
-	private int   _timer;
+	private Timer _timer;
 	private boolean leave;
+	private boolean firstTimeUpdate;
 	
 	@Override
-	public int getID() {
+	public int getID()
+	{
 		return Main.States.SplashState.ordinal();
 	}
 
 	@Override
-	public void init(GameContainer container, StateBasedGame game)
-			throws SlickException {
+	public void init(GameContainer container, StateBasedGame game) throws SlickException
+	{
 		_splashImage = new Image("data/images/teamamerica_1024.png");
 		_splashTheme = new Music("data/sounds/Splash.ogg");
-		_timer = 160;
+
 	}
 	
 	@Override
@@ -36,11 +40,13 @@ public class SplashState extends BHGameState {
 	{
 		super.enter(container, game);
 		this.leave = false;
-		_splashTheme.play();
+		_timer = new Timer();
+		this.firstTimeUpdate = true;
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics graphics) throws SlickException {
+	public void render(GameContainer container, StateBasedGame game, Graphics graphics) throws SlickException
+	{
 		float centerX = container.getWidth() / 2.0f;
 		float centerY = container.getHeight() / 2.0f;
 
@@ -49,19 +55,21 @@ public class SplashState extends BHGameState {
 	}
 
 	@Override
-	public void update(GameContainer container, StateBasedGame game, int millis) throws SlickException {
-		_timer -= millis;
-
-		if (!_splashTheme.playing() || this.leave)
+	public void update(GameContainer container, StateBasedGame game, int millis) throws SlickException
+	{
+		if (_timer.xMilisecondsPassed(1000))
 		{
-			game.enterState(Main.States.MainMenuState.ordinal(), 
-					new FadeOutTransition(), new FadeInTransition());
+			if (!_splashTheme.playing() || this.leave)
+			{
+				game.enterState(Main.States.MainMenuState.ordinal(), new FadeOutTransition(), new FadeInTransition());
+			}
 		}
 		
-		// if (_timer < 0) {
-		// game.enterState(Main.States.MainMenuState.ordinal(),
-		// new FadeOutTransition(), new FadeInTransition());
-		// }
+		if (this.firstTimeUpdate)
+		{
+			_splashTheme.play();
+			this.firstTimeUpdate = false;
+		}
 	}
 	
 	@Override
